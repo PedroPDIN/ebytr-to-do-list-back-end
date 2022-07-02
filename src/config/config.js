f740a196c5d50a100cb2bde91a89e1f851029e87
Pedro Lima <pedro.oficial1969@gmail.com>
2022-07-02 10:17:29 -0300

    doc: instalação do ESlint.

--- src/config/config.js
+++ src/config/config.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_NAME,
+    host: process.env.MYSQL_HOSTNAME,
+    dialect: "mysql"
   },
-  "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_NAME,
+    host: process.env.MYSQL_HOSTNAME,
+    dialect: "mysql"
   },
-  "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_NAME,
+    host: process.env.MYSQL_HOSTNAME,
+    dialect: "mysql"
   }
 }
---
+++ src/migrations/20220701211718-tasks.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', { 
+      id: {
+        allowNull: false,
+        autoInclement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+       },
+
+       task: {
+        allowNull: false,
+        type: Sequelize.STRING,
+       },
+
+       creationDate: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'creation_date',
+       },
+
+       status: {
+        allowNull: false,
+        type: Sequelize.STRING,
+       }
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Tasks');
+  }
+};
