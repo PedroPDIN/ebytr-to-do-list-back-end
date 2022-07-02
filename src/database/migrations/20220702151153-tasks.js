055ff085bef6d1a4472f5b633ef813eaaa2fa788
Pedro Lima <pedro.oficial1969@gmail.com>
2022-07-02 12:27:12 -0300

    feat(database): criação da "migration".

---
+++ src/database/migrations/20220702151153-tasks.js
@@ -0,0 +1,32 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoInclement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+
+      task: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+
+      creationDate: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'creation_date',
+      },
+
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('users');
+  },
+};
